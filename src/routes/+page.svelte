0729592941e3404b4fd612fa5124f1079bb8b728
Gus Ruben <gusruben@protonmail.com>
2024-10-26 00:56:21 -0400

    Add hands texture

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
     </div>
     
     <div class="flex flex-col gap-5 justify-center">
-        <h1 class="font-sans font-bold text-gray-800S text-[8rem] uppercase text-center leading-none h-12">Find Your Fortune</h1>
+        <h1 class="font-sans font-bold text-gray-800 text-[8rem] uppercase text-center leading-none h-12">Find Your Fortune</h1>
         <h1 class="font-sans text-white text-[6rem] text-center leading-none z-10">
             Text <span class="font-bold text-transparent bg-gradient-to-br from-brand-orange to-brand-red bg-clip-text">{formatPhoneNumber(phoneNumber)}</span>
         </h1>
@@ -39,8 +39,11 @@
         <div class="bg-gray-900 w-2/3 mx-auto my-5 p-5 rounded-lg border-gray-800 border-2 text-3xl scale-[90%] text-white">{fortune[0]}: {fortune[1]}</div>
     {/each}
 
-    <div class="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-[40%] w-[90vh] h-[90vh]">
-        <CrystalBall />
+    <img src="hands-on-ball1.png" alt="" class="absolute h-[80%] -bottom-[20%] left-1/2 -translate-x-1/2">
+    <div class="scale-75 translate-y-20">
+        <div class="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-[40%] w-[90vh] h-[90vh]">
+            <CrystalBall />
+        </div>
     </div>
 </div>
 
