29a34eed2d8ea068259412f6c21a753420cb0245
Gus Ruben <gusruben@protonmail.com>
2024-10-29 12:44:19 -0400

    Fix fortune typo

--- src/lib/shared.svelte.ts
+++ src/lib/shared.svelte.ts
@@ -2,7 +2,7 @@ export let fortunes = $state([
     { name: "Josh", fortune: "Your next dentist appointment will reveal more than just cavities.", numberName: false },
     { name: "Jonah", fortune: "A splinter is in your future.", numberName: false },
     { name: "2839", fortune: "Your next haircut will bring unexpected consequences. The barber knows more than they let on.", numberName: true },
-    { name: "Gus", fortune: "You are not illit  erate.", numberName: false },
+    { name: "Gus", fortune: "You are not illiterate.", numberName: false },
     { name: "2938", fortune: "You are lucky to have lived this long.", numberName: true },
     { name: "9824", fortune: "Error 404: Fortune not found", numberName: true },
     { name: "Alex", fortune: "Nose hairs will become a problem.", numberName: false },
